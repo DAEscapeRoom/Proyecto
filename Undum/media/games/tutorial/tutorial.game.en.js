b78225141cc1b52e31cb41ab2dff5da12a00ddc3
PedroReyesMora <61153752+PedroReyesMora@users.noreply.github.com>
2020-04-24 21:55:15 +0200

    Eliminar Comentarios
    Comentarios eliminados del código.

--- Undum/media/games/tutorial/tutorial.game.en.js
+++ Undum/media/games/tutorial/tutorial.game.en.js
@@ -1,32 +1,14 @@
-// ---------------------------------------------------------------------------
-// Edit this file to define your game. It should have at least four
-// sets of content: undum.game.situations, undum.game.start,
-// undum.game.qualities, and undum.game.init.
-// ---------------------------------------------------------------------------
 
-/* A unique id for your game. This is never displayed. I use a UUID,
- * but you can use anything that is guaranteed unique (a URL you own,
- * or a variation on your email address, for example). */
 undum.game.id = "be1c95b9-cbc7-48c6-8e6a-89837aa9113e";
 
-/* A string indicating what version of the game this is. Versions are
- * used to control saved-games. If you change the content of a game,
- * the saved games are unlikely to work. Changing this version number
- * prevents Undum from trying to load the saved-game and crashing. */
 undum.game.version = "1.0";
 
-/* A variable that changes the fade out speed of the option text on
- * a mobile. */
 undum.game.mobileHide = 2000
 
-/* A variable that changes the options fade out speed. */
 undum.game.fadeSpeed = 1500
 
-/* A variable that changes the slide up speed after clicking on an
- * option. */
 undum.game.slideUpSpeed = 500
 
-/* The situations that the game can be in. Each has a unique ID. */
 undum.game.situations = {
     start: new undum.SimpleSituation(
         "<h1>Escena 1: Comienza el juego</h1>\
@@ -296,13 +278,11 @@ undum.game.situations = {
 };
 
 // ---------------------------------------------------------------------------
-/* The Id of the starting situation. */
+
 undum.game.start = "start";
 
 // ---------------------------------------------------------------------------
-/* Here we define all the qualities that our characters could
- * possess. We don't have to be exhaustive, but if we miss one out then
- * that quality will never show up in the character bar in the UI. */
+
 undum.game.qualities = {
     piezas: new undum.NumericQuality(
         "Piezas de llave", {priority:"0001", group:'inventario'}
@@ -324,11 +304,7 @@ undum.game.qualities = {
 };
 
 // ---------------------------------------------------------------------------
-/* The qualities are displayed in groups in the character bar. This
- * determines the groups, their heading (which can be null for no
- * heading) and ordering. QualityDefinitions without a group appear at
- * the end. It is an error to have a quality definition belong to a
- * non-existent group. */
+
 undum.game.qualityGroups = {
     stats: new undum.QualityGroup(null, {priority:"0001"}),
     progress: new undum.QualityGroup('Progress', {priority:"0002"}),
@@ -336,8 +312,7 @@ undum.game.qualityGroups = {
 };
 
 // ---------------------------------------------------------------------------
-/* This function gets run before the game begins. It is normally used
- * to configure the character at the start of play. */
+
 undum.game.init = function(character, system) {
     system.setQuality("alicates",false);
 	character.qualities.piezas = 0;
